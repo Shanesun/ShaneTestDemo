4c86928823667bb853dfe145379e56a619725cf8
Shane <Shane@Shane-Mac-mini.local>
2016-12-08 18:47:28 +0800

    mod swiftDemo

--- TestSwift.playground/Contents.swift
+++ TestSwift.playground/Contents.swift
@@ -30,14 +30,7 @@ dictionary["key1"]
 let emptyArray = [String]()
 let emptyDictionary = [String:String]()
 
-// 控制流 for in
-let numbersArray = [1,2,3,4,5]
-var totalNumber = 0
-for number in numbersArray {
-    totalNumber += 1
-}
-print(totalNumber)
-
+// 显式类型转换
 var optionarlString: String? = "Hello"
 print(optionarlString == nil)
 
@@ -67,6 +60,241 @@ case let x where x.hasSuffix("pepper"):
 default:
     print("everything tastes good in soup")
 }
+
+// for in
+// 1
+let numbersArray = [1,2,3,4,5]
+var totalNumber = 0
+for number in numbersArray {
+    totalNumber += 1
+}
+print(totalNumber)
+// 2
+let interestingNumber = [
+    "Prime":[2,3,4,5,6],
+    "Fibonacci":[1,2,2,3,4],
+    "square":[1,4,6,8,25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumber {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+// while
+var n = 2
+while n<100 {
+    n = n * 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m = m*2
+}while m<100
+print(m)
+
+var total = 0
+for i in 0..<4 { // ..< 不包含上界， .. 包含上界
+    total += 1
+}
+print(total)
+
+// 函数和闭包
+func greet(person:String, day:String)->String{
+    return "Hello \(person), totday is \(day)"
+}
+greet(person: "bob", day: "tue")
+// -省略参数标签
+func greet2(_ person:String, day:String) -> String{
+    return "Hello2 \(person), today is \(day) "
+}
+greet2("json", day: "wend");
+// -元组
+func calculateStatistics(scores:[Int]) -> (min:Int, max:Int, sum:Int){
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score>max {
+            max = score
+        }
+        else if score < min{
+            min = score
+        }
+        
+        sum += score
+    }
+
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics(scores: [24,53,3,2,99])
+print(statistics.sum)
+print(statistics.2)
+// -可变参数
+func sumOf(numbers:Int...) -> Int{
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    
+    return sum
+}
+
+sumOf()
+sumOf(numbers: 42,434,1)
+// -嵌套函数
+func returnFiftent() -> Int{
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFiftent()
+// -返回函数
+func makeIncrementer() -> ((Int) -> Int){
+    func addOne(number:Int) -> Int{
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+// -函数参数
+func hasAnyMatches(list:[Int], condition:(Int) -> Bool) -> Bool{
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(_ number:Int) -> Bool{
+    return number < 10
+}
+var numbers = [20,19,7,12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+// -闭包
+numbers.map { (nubmer:Int) -> Int in
+    let result = 3 * nubmer
+    return result
+}
+// -单句闭包
+let mappedNumbers = numbers.map({number in 3 * number})
+print(mappedNumbers)
+// -尾随闭包 省略括号
+let sortedNumbers = numbers.sorted{$0>$1}
+print(sortedNumbers)
+
+// 对象和类
+class Shape {
+    var numberOfSides = 0
+    func simpleDescreption() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescreption()
+// 构造函数 初始化方法
+class NameShape {
+    var numberOfSides:Int = 0
+    var name:String
+    
+    init(name:String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides"
+    }
+}
+// -override
+class Square: NameShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name:String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)"
+    }
+}
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+// -getter and setter
+class EquilateralTriangle: NameShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength;
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get{
+            return 3.0 * sideLength
+        }
+        set{
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triagle with sides of length \(sideLength)"
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.perimeter)
+
+// willSet didSet
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet{
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square:Square {
+        willSet{
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size:Double, name:String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size:10, name:"another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.square.sideLength)
+
+
+
+
 
 
 

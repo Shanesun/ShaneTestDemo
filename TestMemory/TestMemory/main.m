7baece51521783a3a37959b41422bd9163412230
Shane <Shane@Shane-Mac-mini.local>
2017-01-19 19:41:29 +0800

    TestMemory

--- TestMemory/TestMemory/main.m
+++ TestMemory/TestMemory/main.m
@@ -11,6 +11,12 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        NSError *error = [NSError new];
+        error = nil;
+        
+        NSString *hello1 = @"hello";
+        NSString *hello2 = @"hello";
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }

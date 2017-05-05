a1fa44205e66233e2c9f0f4dc7ce43f48e49aa59
Shane <Shane@Shane-Mac-mini.local>
2017-05-05 17:15:09 +0800

    Improve test code

--- TestUIKit/TestUIKit/WebView/TestWebViewMemeryViewController.m
+++ TestUIKit/TestUIKit/WebView/TestWebViewMemeryViewController.m
@@ -9,12 +9,14 @@
 #import "TestWebViewMemeryViewController.h"
 #import <WebKit/WebKit.h>
 
+#define kTestUrlStr @"http://192.168.31.125/testlargweb/index"
+
 @interface TestWebViewMemeryViewController ()
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIButton *startRequestButton;
 
-@property (strong, nonatomic) UIWebView *webView;
+@property (strong, nonatomic) UIWebView *uiWebView;
 @property (strong, nonatomic) WKWebView *wkWebView;
 
 @end
@@ -24,17 +26,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self createWebView];
+}
+
+- (void)createWebView
+{
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, 40000);
+    
     // UIWebView
     if (self.startRequestButton.tag == 0) {
+        self.title = @"测试UIWebView";
+        self.uiWebView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 40000)];
+        self.uiWebView.scalesPageToFit = YES;
+        [self.scrollView addSubview:self.uiWebView];
         
     }// WKWebView
     else if (self.startRequestButton.tag == 1){
+        self.title = @"测试WKWebView";
         WKWebViewConfiguration *webConfig = [WKWebViewConfiguration new];
-        self.wkWebView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 10000) configuration:webConfig];
-        self.wkWebView.UIDelegate = self;
+        self.wkWebView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 40000) configuration:webConfig];
+        self.wkWebView.scrollView.zoomScale = 1.5;
         [self.scrollView addSubview:self.wkWebView];
-        
-        self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, 10000);
     }
 }
 
@@ -43,7 +55,31 @@
     NSLog(@"--------- didReceiveMemoryWarning ----------");
 }
 
-- (IBAction)startRequestButtonClicked:(UIButton *)sender {
+-(void) dealloc
+{
+    if (self.uiWebView) {
+        // 释放UIWebView资源
+        [self.uiWebView loadHTMLString:@"" baseURL:nil];
+        [self.uiWebView stopLoading];
+        self.uiWebView.delegate = nil;
+        [self.uiWebView removeFromSuperview];
+        self.uiWebView = nil;
+    }
+    else if (self.wkWebView){
+        
+    }
+}
+
+- (IBAction)startRequestButtonClicked:(UIButton *)sender
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:kTestUrlStr]];
+    // uiwebview
+    if (sender.tag == 0) {
+        [self.uiWebView loadRequest:request];
+    }// wkwebview
+    else if (self.startRequestButton.tag == 1){
+        [self.wkWebView loadRequest:request];
+    }
 }
 
 @end
